dde01027f52a9590e68f9db3208a3e88ce09cf98
hrithikakhil <hrithikakhil18@gmail.com>
2024-08-30 00:13:49 +0530

    Spring Security - In Memory Authentication (Roles Based)

--- securityDemo/src/main/java/com/example/securityDemo/controllers/GreetingController.java
+++ securityDemo/src/main/java/com/example/securityDemo/controllers/GreetingController.java
@@ -1,12 +1,26 @@
 package com.example.securityDemo.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+
     @GetMapping("/hello")
     public String sayHello() {
+        return "Hello";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndpoint() {
+        return "Hello";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndpoint() {
         return "Hello";
     }
 }
